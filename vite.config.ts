f211eb869db766e236a064a3480b3b7c2d929da1
andy <Andy.xia@medi-plus.com.cn>
2026-04-02 10:43:59 +0800

    feat: add vercel fallback

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import path from 'path'
 export default defineConfig({
   // GitHub Pages 部署时需要设置 base 为仓库名路径
   // 本地开发时通过环境变量 VITE_APP_BASE 不设置（默认为 '/'）
-  base: process.env.VITE_APP_BASE ?? '/',
+  // base: process.env.VITE_APP_BASE ?? '/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
